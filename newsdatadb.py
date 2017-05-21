c2b7a8981c5541a0c6bb928d4d77d88a9ac345b6
David Gee <davidlguida@gmail.com>
2017-05-21 14:00:45 -0400

    removes unnecessary files

--- newsdatadb.py
+++ newsdatadb.py
@@ -8,13 +8,13 @@ def get_posts():
   """Return all posts from the 'database', most recent first."""
   db = psycopg2.connect(database=DBNAME)
   c = db.cursor()
-  c.execute("select content, time from posts order by time desc")
+  c.execute("select title, name from articles join authors on article.author = authors.id;")
   posts = c.fetchall()
   db.close()
   return posts
 
 def add_post(content):
-  """Add a post to the 'database' with the current timestamp."""
+  """select * from authors;"""
   db = psycopg2.connect(database=DBNAME)
   c = db.cursor()
   c.execute("insert into posts values (%s)", (bleach.clean(content),))  # good
